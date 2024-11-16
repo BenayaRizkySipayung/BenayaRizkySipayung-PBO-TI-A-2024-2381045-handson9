6d236b8ca2be0dd4725f3c4b55b599f970f68901
BenayaRIzkySipayung <2381045@unai.edu>
2024-11-16 23:44:24 +0700

    handson9

---
+++ Main.java
@@ -0,0 +1,19 @@
+import config.Database;
+import repositories.TodoListRepository;
+import repositories.todoListRepositoryDbImpl; // Ensure the correct implementation class is imported
+import services.TodoListService;
+import services.TodoListServiceImpl;
+import views.TodoListTerminalView;
+import views.TodoListView;
+
+public class Main {
+    public static void main(String[] args) {
+        Database database = new Database("my_database", "root", "your_password_here", "localhost", "3306");
+        database.setup();
+
+        TodoListRepository todoListRepository = new todoListRepositoryDbImpl(database); // Corrected class name
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListTerminalView(todoListService);
+        todoListView.run();
+    }
+}
